05a2105a5dc57fde99bf3f4c6f42827eae32c8f1
Manas Bhatia <manasbhatiaprsnl@gmail.com>
2025-01-01 16:02:30 +0530

    usermodel, videomodel, hooks, JWT

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,25 @@
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => {() => {}} // somewhat aisa sa system hai
-/*
-const asyncHandler = (fn) => async (req, res, next) => {
-    try{
-        await fn(req, res, next);
-    }
-    catch(error){
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message,
-        })
-    }
-}
-    */
 
-const asyncHandler(requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-    }
-}
+// this function is accepting a function fn and returning an async function which takes parameters (req, res, next)
+// basically jb hum iss function mein apna connectDatabase function pass karenge toh hume uska error manually handle krne ki jarurat nhi padegi
+const asyncHandler = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: false,
+        message: error.message,
+      });
+    };
+  };
+};
 
-export {asyncHandler}
+// const asyncHandler(requestHandler) => {
+//     (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//     }
+// }
+
+export { asyncHandler };
