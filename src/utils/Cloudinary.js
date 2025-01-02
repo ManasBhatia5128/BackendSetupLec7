ee4d18b2d13db265d02f34bbeadb5e19e9ea3b69
Manas Bhatia <manasbhatiaprsnl@gmail.com>
2025-01-02 13:49:20 +0530

    cloudinary

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs"; // fs is file system used to perform file read write etc information
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+}); // ye delete krna hai ki nhi?
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath){
+            throw Error("File path is not provided");
+        }
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto", // check type of file
+        }); // public id, name etc upload options can be further given
+        console.log("file has been uploaded successfully");
+        console.log(response);
+        return response;
+        // file has been uploaded successfully
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath); // removes locally saved temporary files synchronously as upload operation got failed
+        console.log(error);
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary};
