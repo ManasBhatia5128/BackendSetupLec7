f19b273c4db4f51137ae20d483467f7b033fe1b3
Manas Bhatia <manasbhatiaprsnl@gmail.com>
2025-01-02 14:40:40 +0530

    multer completed

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,1 +1,12 @@
 import multer from "multer";
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {  // file access mil jaata hai multer ke saath, 
+      cb(null, './public/temp');
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname); // not a good practice
+    }
+  })
+  
+export const upload = multer({ storage: storage }) // can be replaced with storage only in ES6
