2e1ec0dbd6516f42da453f025f4042def8b74c57
Manas Bhatia <manasbhatiaprsnl@gmail.com>
2025-01-17 15:25:03 +0530

    register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { APIError } from "../utils/APIError.js";
-import {APIResponse} from "../utils/APIResponse.js"
+import { APIResponse } from "../utils/APIResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
 9. return response
    */
 
-  const { username, fullname, email, password } = req.body;
+  const { username, fullname, email, password } = req.body; // express method used to get user data from form
   console.log(username);
   console.log(email);
   console.log(password);
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new APIError(400, "Email must be valid");
   }
 
-  if (password.length() < 8) {
+  if (password.length < 8) {
     throw new APIError(400, "Password must contain at least 8 characters");
   }
 
@@ -40,7 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new APIError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     // pehla user jo mila if exists
     $or: [{ username }, { email }],
   });
@@ -67,8 +67,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new APIError(400, "Avatar file is required");
   }
 
-  await User.create({ // time required in creation
-    fullName,
+  await User.create({
+    // time required in creation
+    fullname,
     username,
     email,
     password,
@@ -79,15 +80,17 @@ const registerUser = asyncHandler(async (req, res) => {
   // iss se ek database call extra jaa rahi hai but confirm ho jayega ki user create hua hai ki nhi
   // also iss se hum select kr paa rhe hain ki konse fields nhi chaiye (though .create wale mein bhi kr skte the like password: undefined)
 
-  const createdUser = await User.findById(username._id).select("-password -refreshToken") // _id mongoDB bana leta hai har field ki
-// jo fields nhi chaiye vo likh do
+  const createdUser = await User.findById(username._id).select(
+    "-password -refreshToken"
+  ); // _id mongoDB bana leta hai har field ki
+  // jo fields nhi chaiye vo likh do
 
-  if(!createdUser){
-    throw new APIError(500, "Something went wrong while registering the user") // or "Internal server error"
+  if (!createdUser) {
+    throw new APIError(500, "Something went wrong while registering the user"); // or "Internal server error"
   }
-  return res.status(201).json(
-    new APIResponse(200, createdUser, "User registered successfully");
-  )
+  return res
+    .status(201)
+    .json(new APIResponse(201, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
